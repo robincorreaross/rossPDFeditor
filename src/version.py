c26b6b5b8ae138732cc0e6161b0ea58f69a35423
robincorreaross <robincorrea@gmail.com>
2026-03-21 07:46:01 -0300

    Correção de Auto-Update: Restaurando links de verificação em version.py v1.8.2.1

--- src/version.py
+++ src/version.py
@@ -1,4 +1,9 @@
 APP_VERSION = "1.8.2"
+APP_NAME = "RossPDFEditor"
+UPDATE_URL = "https://raw.githubusercontent.com/robincorreaross/rossPDFeditor/master/version.json"
+DOWNLOAD_URL = "https://github.com/robincorreaross/rossPDFeditor/releases/latest"
+DOWNLOAD_ZIP_URL = "https://github.com/robincorreaross/rossPDFeditor/releases/latest/download/RossPDFEditor.zip"
+
 SUPABASE_URL = ""
 SUPABASE_KEY = ""
 SUPABASE_TABLE = ""
